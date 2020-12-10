a32a77c16aab593e8b0443db97d8ff416de0cfc7
Ajay Prajapati <74779607+ajaypra0518@users.noreply.github.com>
2020-12-10 11:03:54 +0530

    Add files via upload

---
+++ osms.js
@@ -0,0 +1,77 @@
+var i=0,k=0;
+$(document).ready(function(){
+  var interval = setInterval(rotateSlides, 3000); 
+  function rotateSlides(){
+  
+
+    if(i==2){
+      $('#sl3').css('color','#FF1493')
+      $('#sl1').css('color','transparent')
+      $('#sl2').css('color','transparent')
+      i=0;
+      k=1;
+    }
+    if(i==1){
+      $('#sl2').css('color','#FF1493')
+      $('#sl1').css('color','transparent')
+      i++;
+    }
+    if(i==0 && k==0){
+      $('#sl1').css('color','#FF1493')
+      $('#sl3').css('color','transparent')
+      i++;
+    }
+    k=0;
+
+   
+    var firstSlide = $('#carousel').find('#imgmain img:first');
+    var width = firstSlide.width();
+  
+    firstSlide.animate({marginLeft: -width }, 700, function(){
+      var lastSlide = $('#carousel').find('#imgmain img:last');
+      lastSlide.after(firstSlide);
+      firstSlide.css({marginLeft: 0});
+    });
+  }
+
+
+
+$('#right-arrow').click(previousSlide);
+$('#left-arrow').click(nextSlide);
+
+
+$('#sl1,#sl2,#sl3').click(function(){
+  clearInterval(interval);
+  console.log("yess")
+    $('.icon').css('color','transparent')
+  var mid=this.id;
+  console.log(mid)
+  $(`#${mid}`).css('color','#FF1493');
+ 
+  interval = setInterval(rotateSlides, 3000);
+  previousSlide();
+   
+});
+
+
+function previousSlide(){
+ 
+  console.log(interval)
+  clearInterval(interval);
+  var currentSlide = $('#carousel').find('#imgmain img:first');
+  var width = currentSlide.width();
+  var previousSlide = $('#carousel').find('#imgmain img:last')
+  previousSlide.css({marginLeft: -width})
+  currentSlide.before(previousSlide);
+  previousSlide.animate({marginLeft: 0}, 700, function(){
+    interval = setInterval(rotateSlides, 3000);
+  });
+}
+
+function nextSlide(){
+clearInterval(interval);
+  rotateSlides()
+  interval = setInterval(rotateSlides, 3000);
+}
+
+});
